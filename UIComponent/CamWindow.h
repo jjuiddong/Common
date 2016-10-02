7ba91ae8129cd7a6b53277e016c4735fe5744330
jjuiddong <jjuiddong@gmail.com>
2016-10-02 11:14:02 +0900

    update UIComponent, CamWindow, zoom in/out

--- UIComponent/CamWindow.h
+++ UIComponent/CamWindow.h
@@ -19,11 +19,20 @@ protected:
 
 
 public:
+	cv::Mat m_image;
 	Gdiplus::Bitmap *m_bitmap;
 	cv::Size m_imageSize;
 	int m_lastRenderTime;
+	Gdiplus::Rect m_bound;
+	Gdiplus::Font m_Font;
+	Gdiplus::SolidBrush m_Brush;
+	Gdiplus::SolidBrush m_grayBrush;
+
+	// double buffering
+	Gdiplus::Bitmap *m_memBmp;
 
 	// rectangle draw
+	bool m_dragImage;
 	bool m_isStartRect;
 	bool m_isClick;
 	CPoint m_clickPos;
@@ -37,5 +46,6 @@ public:
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
 };
 
