d69cb389f3be548a47958ba7ac599a0a53ea0202
jjuiddong <jjuiddong@gmail.com>
2018-10-15 08:26:01 +0900

    update compile error bugfix

--- AI/action/rootaction.h
+++ AI/action/rootaction.h
@@ -6,13 +6,13 @@
 
 namespace ai
 {
-	template<class T>
 
+	template<class T>
 	class cRootAction : public cAction<T>
 	{
 	public:
  		cRootAction(T *agent = NULL)
- 			: cAction(agent, "Root", "", eActionType::ROOT)
+ 			: cAction<T>(agent, "Root", "", eActionType::ROOT)
  		{
  		}
 	};
