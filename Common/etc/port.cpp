089ec4dbe94ebc9daa6e11a64871b3f5fd43c8ad
jjuiddong <jjuiddong@gmail.com>
2019-06-27 09:42:40 +0900

    update com port enum

--- Common/etc/port.cpp
+++ Common/etc/port.cpp
@@ -9,7 +9,16 @@ using namespace common;
 // 
 bool common::EnumCOMPort(OUT vector<std::pair<UINT, string>> &out)
 {
+
+#ifdef _UNICODE
+	vector<std::pair<UINT, wstring>> result;
+	CEnumerateSerial enumSerial;
+	enumSerial.UsingSetupAPI2(result);
+	for (auto &v : result)
+		out.push_back({ v.first, wstr2str(v.second) });
+#else
 	CEnumerateSerial enumSerial;
 	enumSerial.UsingSetupAPI2(out);
+#endif
 	return true;
 }
