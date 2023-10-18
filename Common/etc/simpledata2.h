af855f9159e2e6f66a794e95311d51a55930f089
jjuiddong <jjuiddong@hanmail.net>
2023-10-18 19:20:32 +0900

    update common simpledata, update comment

--- Common/etc/simpledata2.h
+++ Common/etc/simpledata2.h
@@ -2,7 +2,7 @@
 // 2019-11-19, jjuiddong
 // simple data read/write class
 //
-// sample data file
+// sample data 1
 // str0 (state change: state0 -> state1)
 //		str1 str2 (state1)
 //		str3 str4 (state1)
@@ -24,7 +24,47 @@
 //	rules.push_back({ 2, "str6", 3, 1 });
 //	rules.push_back({ 2, "str0", 1, 0 });
 //	rules.push_back({ 3, "str0", 1, 0 });
+// -----------------------------------------------------------------------
+// sample data 2
+//link
+//	name base
+//	geometry box
+//	scale 1 1 1
+//link
+//	name arm1
+//	geometry box
+//	scale 1 1 1
+//link
+//	name arm2
+//	geometry box
+//	scale 1 1 1
+//joint
+//	name joint1
+//	type revolute
+//	parent base
+//	child arm1
+//	origin_rpy 0 0 0
+//	origin_xyz 0 0 0
+//	axis_xyz 0 1 0
+// joint
+//	name joint2
+//	type revolute
+//	parent arm1
+//	child arm2
+//	origin_rpy 0 0 0
+//	origin_xyz 0 0 0
+//	axis_xyz 0 1 0
 //
+//rule
+// vector<cSimpleData2::sRule> rules;
+// rules.push_back({ 0, "link", 1, 0 });
+// rules.push_back({ 1, "link", 1, 0 });
+// rules.push_back({ 2, "link", 1, 0 });
+// rules.push_back({ 0, "joint", 2, 0 });
+// rules.push_back({ 1, "joint", 2, 0 });
+// rules.push_back({ 2, "joint", 2, 0 });
+// -----------------------------------------------------------------------
+
 #pragma once
 
 
@@ -72,11 +112,11 @@ namespace common
 	//---------------------------------------------------------------------------------------
 	// template function
 
-#define FIND_ATTR(node, key)				\
+#define FIND_ATTR(node, key, count)			\
 		auto it = node->attrs.find(key);	\
 		if (node->attrs.end() == it)		\
 			return defaultValue;			\
-		if (it->second.size() <= 1)			\
+		if (it->second.size() <= count)		\
 			return defaultValue;
 
 
@@ -84,7 +124,7 @@ namespace common
 	inline const T cSimpleData2::Get(cSimpleData2::sNode *node
 		, const string &key, const T &defaultValue)
 	{
-		FIND_ATTR(node, key);
+		FIND_ATTR(node, key, 1);
 		_variant_t v = (T)0;
 		v = common::str2variant(v.vt, it->second[1]);
 		return (T)v;
@@ -95,8 +135,20 @@ namespace common
 	inline const string cSimpleData2::Get(cSimpleData2::sNode *node
 		, const string &key, const string &defaultValue)
 	{
-		FIND_ATTR(node, key);
+		FIND_ATTR(node, key, 1);
 		return it->second[1];
 	}
 
+	// template specialization (Vector3)
+	template<>
+	inline const Vector3 cSimpleData2::Get(cSimpleData2::sNode* node
+		, const string& key, const Vector3& defaultValue)
+	{
+		FIND_ATTR(node, key, 3);
+		Vector3 v;
+		v.x = (float)atof(it->second[1].c_str());
+		v.y = (float)atof(it->second[2].c_str());
+		v.z = (float)atof(it->second[3].c_str());
+		return v;
+	}
 }
